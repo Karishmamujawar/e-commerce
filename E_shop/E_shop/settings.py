fc327cb4139b118ea648697d7bb8a9e27da039af
Karishmamujawar <151314602+Karishmamujawar@users.noreply.github.com>
2025-07-08 22:15:02 +0530

    updated

--- E_shop/E_shop/settings.py
+++ E_shop/E_shop/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+#DEBUG=False
 
-ALLOWED_HOSTS = ['e-commerce.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'e-commerce.onrender.com']
 
 
 # Application definition
